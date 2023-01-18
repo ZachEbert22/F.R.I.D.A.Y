16498b128ba50f364c44ee01727e532ceb4e7dfe
Golfing7 <atroo@me.com>
2023-01-18 10:28:24 -0500

    Move shutdown to user space

--- kernel/comhand.c
+++ kernel/comhand.c
@@ -9,17 +9,24 @@
 #include "mpx/comhand.h"
 #include "commands.h"
 
-#define CMD_PROMPT ">> "
-
 /**
  * @brief An array of pointers to command functions. These functions
  * will return true if the command was handled by them, or false if not.
  */
 bool (*comm_funcs[])(const char *comm) = {
-        &version,
-        &help,
-        &get_time_menu
+        &cmd_version,
+        &cmd_help,
+        &cmd_get_time_menu,
+        &cmd_shutdown
 };
+
+/// Used to denote if the comm hand should stop.
+bool sig_shutdown = false;
+
+void signal_shutdown(void)
+{
+        sig_shutdown = true;
+}
 
 void comhand(void)
 {
@@ -30,7 +37,7 @@ void comhand(void)
         println("=> Get Time");
         println("=> version");
         println("=> shutdown");
-        for(;;)
+        while(!sig_shutdown)
         {
                 //100 + 1 for the null terminator.
                 char buf[101] = {0};
@@ -41,18 +48,6 @@ void comhand(void)
                 //Handle all functions.
                 int comm_func_count = sizeof (comm_funcs) /
                         sizeof (comm_funcs[0]);
-
-                if (strcicmp(buf, "shutdown") == 0){
-                        println("Are you sure you want to shutdown? enter y for yes or N for no");
-                        memset(buf,'\0', 101);
-                        print(CMD_PROMPT);
-                        sys_req(READ, COM1, buf, 5);
-                        if (strcicmp(buf, "y") == 0 || strcicmp(buf, "yes") == 0){
-                                return;
-                        }else{
-                                continue;
-                        }
-                }
 
                 for (int i = 0; i < comm_func_count; ++i)
                 {
--- user/commands.c
+++ user/commands.c
@@ -5,15 +5,17 @@
 #include "commands.h"
 #include "string.h"
 #include "stdio.h"
+#include "sys_req.h"
+#include "mpx/comhand.h"
 #include "mpx/get_set_time.h"
 
-bool version(const char *comm)
+bool cmd_version(const char *comm)
 {
         //The command's label.
         const char *label = "version";
 
         //Check if it matched.
-        int cmp = strcmp(comm, label);
+        int cmp = strcicmp(comm, label);
         if(cmp != 0)
                 return false;
 
@@ -23,13 +25,34 @@ bool version(const char *comm)
         return true;
 }
 
-bool get_time_menu(const char *comm)
+bool cmd_shutdown(const char *comm)
+{
+        const char *label = "shutdown";
+
+        int cmp = strcicmp(comm, label);
+        if(cmp != 0)
+                return false;
+
+        println("Are you sure you want to shutdown? enter y for yes or N for no");
+        char confirm_buf[6] = {0};
+        print(CMD_PROMPT);
+        sys_req(READ, COM1, confirm_buf, 5);
+
+        //Check confirmation.
+        if (strcicmp(confirm_buf, "y") == 0 ||
+                        strcicmp(confirm_buf, "yes") == 0){
+                signal_shutdown();
+        }
+        return true;
+}
+
+bool cmd_get_time_menu(const char *comm)
 {
         //The command's label.
         const char *label = "get time";
 
         //Check if it matched.
-        int cmp = strcmp(comm, label);
+        int cmp = strcicmp(comm, label);
         if(cmp != 0)
                 return false;
 
@@ -38,7 +61,7 @@ bool get_time_menu(const char *comm)
         return true;
 }
 
-bool help(const char *comm)
+bool cmd_help(const char *comm)
 {
         //The command's label.
         const char *label = "help";
