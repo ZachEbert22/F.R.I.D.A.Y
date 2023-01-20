3a718b574e0827f69be227fdbda931604994672e
Golfing7 <atroo@me.com>
2023-01-20 08:46:26 -0500

    Move welcome printing

--- kernel/comhand.c
+++ kernel/comhand.c
@@ -27,48 +27,56 @@ bool sig_shutdown = false;
 
 void signal_shutdown(void)
 {
-        sig_shutdown = true;
+    sig_shutdown = true;
+}
+
+/**
+ * @brief Prints a welcome message to the user.
+ */
+void print_welcome(void)
+{
+    println("*********      **********      **      ****                  **         **      **");
+    println("**             **     **       **      **   **             **  **         **   **");
+    println("**             **    **        **      **    **           **     **         ****");
+    println("*****          **  **          **      **     **         **       **         **");
+    println("**             ****            **      **     **        *************        **");
+    println("**             **  **          **      **     **       **           **       ** ");
+    println("**             **    **        **      **    **       **             **      **");
+    println("**             **     **       **      **   **       **               **     **");
+    println("**             **      **      **      ******       **                 **    **");
+
+    println("Welcome to MPX. Please select an option");
+    println("=> Help");
+    println("=> Set Time");
+    println("=> Set Date");
+    println("=> Get Time");
+    println("=> version");
+    println("=> shutdown");
 }
 
 void comhand(void)
 {
-        println("*********      **********      **      ****                  **         **      **");
-        println("**             **     **       **      **   **             **  **         **   **");
-        println("**             **    **        **      **    **           **     **         ****");
-        println("*****          **  **          **      **     **         **       **         **");
-        println("**             ****            **      **     **        *************        **");
-        println("**             **  **          **      **     **       **           **       ** ");
-        println("**             **    **        **      **    **       **             **      **");
-        println("**             **     **       **      **   **       **               **     **");
-        println("**             **      **      **      ******       **                 **    **");
-        
-        println("Welcome to MPX. Please select an option");
-        println("=> Help");
-        println("=> Set Time");
-        println("=> Set Date");
-        println("=> Get Time");
-        println("=> version");
-        println("=> shutdown");
-        while(!sig_shutdown)
+    print_welcome();
+    while (!sig_shutdown)
+    {
+        //100 + 1 for the null terminator.
+        char buf[101] = {0};
+
+        print(CMD_PROMPT);
+        sys_req(READ, COM1, buf, 100);
+
+        //Strip whitespace.
+        str_strip_whitespace(buf, NULL, 0);
+
+        //Handle all functions.
+        int comm_func_count = sizeof(comm_funcs) /
+                              sizeof(comm_funcs[0]);
+
+        for (int i = 0; i < comm_func_count; ++i)
         {
-                //100 + 1 for the null terminator.
-                char buf[101] = {0};
-
-                print(CMD_PROMPT);
-                sys_req(READ, COM1, buf, 100);
-
-                //Strip whitespace.
-                str_strip_whitespace(buf, NULL, 0);
-
-                //Handle all functions.
-                int comm_func_count = sizeof (comm_funcs) /
-                        sizeof (comm_funcs[0]);
-
-                for (int i = 0; i < comm_func_count; ++i)
-                {
-                        bool result = comm_funcs[i](buf);
-                        if(result)
-                                break;
-                }
+            bool result = comm_funcs[i](buf);
+            if (result)
+                break;
         }
+    }
 }
