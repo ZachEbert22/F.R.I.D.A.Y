1feccbd7b0c947f488a8ecab9dbe6f2a4ba033b2
Golfing7 <atroo@me.com>
2023-02-03 08:48:55 -0500

    Introduce PCB struct

--- include/mpx/pcb.h
+++ include/mpx/pcb.h
@@ -1,2 +1,44 @@
 #ifndef MPX_PCB_H
 #define MPX_PCB_H
+
+#define PCB_MAX_NAME_LEN 8
+#define PCB_STACK_SIZE 4096
+
+///The clas of a PCB.
+enum pcb_class {
+    USER = 0,
+    SYSTEM = 1,
+};
+
+///The execution state of a PCB.
+enum pcb_exec_state {
+    READY = 0,
+    RUNNING = 1,
+    BLOCKED = 2,
+};
+
+///An enum of dispatch state for PCBs.
+enum pcb_dispatch_state {
+    SUSPENDED = 0,
+    NOT_SUSPENDED = 1,
+};
+
+///The definition of a process control block.
+struct pcb {
+    ///The name of the PCB, max length of 8.
+    const char *name;
+    ///The process class type.
+    enum pcb_class process_class;
+    ///Integer priority of PCB, 0-9, lower = higher priority;
+    int priority;
+    ///The execution state of this PCB.
+    enum pcb_exec_state exec_state;
+    ///The dispatch state of this PCB.
+    enum pcb_dispatch_state dispatch_state;
+    ///A pointer to the next available byte in the stack.
+    int stack_ptr;
+    ///The stack itself.
+    unsigned char stack[PCB_STACK_SIZE];
+};
+
+#endif
--- kernel/pcb.c
+++ kernel/pcb.c
@@ -9,7 +9,49 @@
 #include "stdlib.h"
 #include "color.h"
 #include "cli.h"
+#include "memory.h"
+#include "mpx/pcb.h"
+#include "linked_list.h"
 
+///The PCB queue.
+static const linked_list *pcb_queue;
+
+struct pcb *pcb_alloc(void)
+{
+    struct pcb *pcb_ptr = sys_alloc_mem(sizeof (struct pcb));
+    return pcb_ptr;
+}
+
+int pcb_free(struct pcb* pcb_ptr)
+{
+    if(pcb_ptr == NULL)
+        return 1;
+
+    return sys_free_mem(pcb_ptr);
+}
+
+struct pcb *pcb_setup(const char *name, int class, int priority)
+{
+    //Don't allow null names or names that are too long.
+    if(name == NULL || strlen(name) > PCB_MAX_NAME_LEN)
+        return NULL;
+
+    //Check validity of class.
+    if(class < USER || class > SYSTEM)
+        return NULL;
+
+    //Check validity of priority.
+    if(priority < 0 || priority > 9)
+        return NULL;
+
+    struct pcb *pcb_ptr = pcb_alloc();
+    if(pcb_ptr == NULL)
+        return NULL;
+
+    pcb_ptr->name = name;
+    pcb_ptr->process_class = class;
+    pcb_ptr->priority = priority;
+}
 //pcb_allocoate
 //pcb_free
 //pcb* pcb_setup
