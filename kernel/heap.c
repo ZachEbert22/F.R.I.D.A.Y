0a5d0c0829a0bb5f9975519db45647f06914b433
Golfing7 <atroo@me.com>
2023-03-24 08:27:27 -0400

    Add memory control block

--- kernel/heap.c
+++ kernel/heap.c
@@ -3,3 +3,24 @@
 //
 
 #include "mpx/heap.h"
+#include "stddef.h"
+
+///A structure that contains memory.
+typedef struct mem_block {
+    ///The previous memory block.
+    struct mem_block *prev;
+    ///The next memory block.
+    struct mem_block *next;
+
+    ///The start address of this block
+    int start_address;
+    ///The size of this block
+    size_t size;
+} mem_block_t;
+
+
+
+void initialize_heap(size_t size)
+{
+
+}
