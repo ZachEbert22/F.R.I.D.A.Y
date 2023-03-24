355dda166a7fb13c2cdddea895351e1e84137777
Golfing7 <atroo@me.com>
2023-03-24 10:12:22 -0400

    Add credits and doxygen

--- include/mpx/heap.h
+++ include/mpx/heap.h
@@ -8,11 +8,26 @@
 #include "stddef.h"
 
 /**
+ * @file heap.h
+ * @brief the heap file contains functions useful for allocating and freeing memory.
+ */
+
+/**
  * Initializes the heap with the given size.
  *
  * @param size the size of the new heap.
+ * @authors Andrew Bowie
  */
 void initialize_heap(size_t size);
+
+/**
+ * Allocates memory to the heap, returns NULL if it can't find enough room for the memory.
+ *
+ * @param size the amount of bytes to allocate.
+ * @return the pointer to the allocated memory, or NULL.
+ * @authors Andrew Bowie, Jared Crowley
+ */
+void *allocate_memory(size_t size);
 
 /**
  * Frees the Memory Block at the given pointer.
